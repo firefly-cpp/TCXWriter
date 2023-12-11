cac1ecbdcb281a880d073397d30056c4eaf1358c
Iztok Fister Jr. <iztok@iztok-jr-fister.eu>
2023-12-11 17:09:49 +0100

    New version of library

--- src/tcxwriter.c
+++ src/tcxwriter.c
@@ -1,70 +1,133 @@
 #include "tcxwriter.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-static FILE *file;
-
-// schemas related content
-const char *xmlSchema = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-  "<TrainingCenterDatabase\n"
-  "  xsi:schemaLocation=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd\"\n"
-  "  xmlns:ns5=\"http://www.garmin.com/xmlschemas/ActivityGoals/v1\"\n"
-  "  xmlns:ns3=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\"\n"
-  "  xmlns:ns2=\"http://www.garmin.com/xmlschemas/UserProfile/v2\"\n"
-  "  xmlns=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2\"\n"
-  "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:ns4=\"http://www.garmin.com/xmlschemas/ProfileExtension/v1\">\n"
-  "  <Activities>\n";
+#include <time.h>
 
 void
-tcx_init (const char *filename, int *error)
+tcx_current_timestamp (char *tcxDatetimeFormat)
+{
+  time_t raw;
+  struct tm *utc;
+
+  // current time
+  time (&raw);
+  utc = gmtime (&raw);		// get UTC
+
+  // Use proper TCX format
+  strftime (tcxDatetimeFormat, 20, "%Y-%m-%dT%H:%M:%SZ", utc);
+}
+
+void
+tcx_add_trackpoint (Track *track, float latitude, float longitude,
+		    float altitude, int hr)
+{
+
+  // Obtain the current timestamp
+  char timestamp[20];
+  tcx_current_timestamp (timestamp);
+
+  // @todo: Check whether the data is null and whether there are other discrepancies before creating a new Trackpoint.
+
+  // New trackpoint
+  Trackpoint new_trackpoint;
+  snprintf (new_trackpoint.timestamp, sizeof (new_trackpoint.timestamp), "%s",
+	    timestamp);
+  new_trackpoint.latitude = latitude;
+  new_trackpoint.longitude = longitude;
+  new_trackpoint.altitude = altitude;
+  new_trackpoint.hr = hr;
+
+  // Resize the array
+  track->points =
+    realloc (track->points, (track->size + 1) * sizeof (Trackpoint));
+  if (track->points != NULL)
+    {
+      track->points[track->size] = new_trackpoint;
+      track->size++;
+    }
+  else
+    {
+      // Report problems when storing
+      printf
+	("Error: Memory allocation failed when adding a new TrackPoint.\n");
+    }
+}
+
+void
+tcx_write_to_file (const char *filename, const Track *track)
 {
   // open a file
-  file = fopen (filename, "w");
+  FILE *file = fopen (filename, "w");
   if (file == NULL)
     {
-      *error = -1;
+      perror ("Error opening file!");
       return;
     }
 
-  // Init a header in file
-  size_t len = strlen (xmlSchema);
-  size_t written = fwrite (xmlSchema, sizeof (char), len, file);
+  // Write TCX header
+  fprintf (file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+  fprintf (file,
+	   "<TrainingCenterDatabase xmlns=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2\"\n");
+  fprintf (file,
+	   "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
+  fprintf (file,
+	   "  xsi:schemaLocation=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd\">\n");
+  fprintf (file, "  <Activities>\n");
+  fprintf (file, "    <Activity Sport=\"Cycling\">\n");	// @todo: Support user-defined entry
+  fprintf (file, "      <Id>%s</Id>\n", track->points[0].timestamp);	// Use the timestamp of the first trackpoint as the activity ID
 
-  // Check for errors during the write process
-  if (written != len)
+  // Write each trackpoint to file
+  for (size_t i = 0; i < track->size; ++i)
     {
-      *error = -1;
-      return;
-      fclose (file);
+      fprintf (file, "      <Trackpoint>\n");
+      fprintf (file, "        <Time>%s</Time>\n", track->points[i].timestamp);
+      fprintf (file, "        <Position>\n");
+      fprintf (file, "          <LatitudeDegrees>%f</LatitudeDegrees>\n",
+	       track->points[i].latitude);
+      fprintf (file, "          <LongitudeDegrees>%f</LongitudeDegrees>\n",
+	       track->points[i].longitude);
+      fprintf (file, "        </Position>\n");
+      fprintf (file, "        <AltitudeMeters>%f</AltitudeMeters>\n",
+	       track->points[i].altitude);
+      fprintf (file, "        <HeartRateBpm>\n");
+      fprintf (file, "          <Value>%d</Value>\n", track->points[i].hr);
+      fprintf (file, "        </HeartRateBpm>\n");
+      fprintf (file, "      </Trackpoint>\n");
     }
-}
 
-void
-tcx_add_trackpoint (uint32_t timestamp, float latitude, float longitude,
-		    float altitude, int heart_rate)
-{
-  // Add a new trackpoint to the TCX file
-  fprintf (file, "<Trackpoint>\n");
-  fprintf (file, "  <Time>%u</Time>\n", timestamp);
+  // Write TCX footer
+  fprintf (file, "    </Activity>\n");
+  fprintf (file, "  </Activities>\n");
+  fprintf (file, "</TrainingCenterDatabase>\n");
 
-  // TODO
-
-  fprintf (file, "</Trackpoint>\n");
-}
-
-void
-tcx_finalize ()
-{
   // Close the file
   fclose (file);
+}
+
+// Free memory at the end
+void
+tcx_free (Track *track)
+{
+  free (track->points);
 }
 
 int
 main ()
 {
-  // TODO
-  int error = 0;
-  tcx_init ("activity.tcx", &error);
+  // A minimal example
+
+  //Track activity = { NULL, 0 };       // Init the track
+
+  // Add two trackpoints
+
+  //tcx_add_trackpoint (&activity, 1, -1, 100.0, 120);
+  //tcx_add_trackpoint (&activity, 1.1, -.2, 101.0, 125);
+
+  //tcx_write_to_file ("activity.tcx", &activity);
+
+  // Free allocated memory
+  //tcx_free (&activity);
 
   return 0;
 }
--- src/tcxwriter.h
+++ src/tcxwriter.h
@@ -1,68 +1,83 @@
 /** @file tcxwriter.h
  *  @brief Function/procedure prototypes for tcxwriter library.
  *
- *  #TODO
- *
- *
+ *  @note Laps are not yet supported in the current version.
  *  @author Iztok Fister Jr.
  *  @bug No known bugs.
+ *  @version v@TCXWRITER_VERSION_MAJOR@.@TCXWRITER_VERSION_MINOR@.@TCXWRITER_VERSION_PATCH@
+*   @date Last Change: 11.12.2023
  */
 #ifndef TCXWRITER_H
 #define TCXWRITER_H
 
+// Version information
+#define TCXWRITER_VERSION_MAJOR 0
+#define TCXWRITER_VERSION_MINOR 1
+#define TCXWRITER_VERSION_PATCH 0
+
+#include <stddef.h>
 #include <stdint.h>
 
-/** @struct trackpoint
- *  @brief The structure represents a trackpoint.
- *  @var trackpoint::timestamp
- *  TODO
- *  @var trackpoint::latitude
- *  TODO
- *  @var trackpoint::longitude
- *  TODO
- *  @var trackpoint::altitude
- *  TODO
- *  @var trackpoint::hr
- *  TODO
+/**
+ * @brief The structure represents a trackpoint with timestamp, location (latitude and longitude), altitude, and heart rate.
+ * @note We should also consider storing cadence and wattmeter values and steps in the future version.
  */
 typedef struct trackpoint
 {
-  uint32_t timestamp;
-  float latitude;
-  float longitude;
-  float altitude;
-  int hr;
-};
+  char timestamp[20]; /**< Timestamp in "YYYY-MM-DDThh:mm:ssZ" format */
+  float latitude;     /**< Latitude of the trackpoint */
+  float longitude;    /**< Longitude of the trackpoint */
+  float altitude;     /**< Altitude of the trackpoint */
+  int hr;	      /**< Heart rate of the trackpoint */
+} Trackpoint;
 
 /**
- * The procedure that initiates (opens) a file for writing content into.
- * @param[in] filename
+ * @brief The structure represents a track with an array of track points and sizes.
  */
-void tcx_init(const char *filename, int* error);
+typedef struct track
+{
+  Trackpoint *points;	/**< Array of trackpoints */
+  size_t size;		/**< Size of the track (number of trackpoints) */
+} Track;
 
 /**
- * Main procedure for writing track points into a file.
- * @param[in] timestamp
- * @param[in] altitude
- * @param[in] longitude
- * @param[in] altitude
- * @param[in] hr
+ * @brief Function to get the current timestamp in TCX format.
+ *
+ * @param[out] tcxFormat Buffer to store the TCX-formatted timestamp.
  */
-void tcx_add_trackpoint(uint32_t timestamp, float latitude, float longitude, float altitude, int hr);
-/**
- * The procedure that writes metadata (e.g., device name) to the TCX file.
- * @param[in] device_name
- */
-void tcx_add_metadata();
-/**
- * Procedure for validating a composed TCX file.
- * @param[in] X
- */
-void tcx_validate();
+void tcx_current_timestamp (char *tcxFormat);
 
 /**
- * Close the file.
+ * @brief Function to add a trackpoint to a track.
+ *
+ * @param[in,out] track Pointer to the track structure.
+ * @param[in] latitude Latitude of the trackpoint.
+ * @param[in] longitude Longitude of the trackpoint.
+ * @param[in] altitude Altitude of the trackpoint.
+ * @param[in] hr Heart rate of the trackpoint.
  */
-void tcx_finalize();
+void tcx_add_trackpoint (Track * track, float latitude, float longitude,
+			 float altitude, int hr);
+
+/**
+ * @brief Function to write track points in TCX format to a file.
+ *
+ * @param[in] filename Name of the TCX file.
+ * @param[in] track Pointer to the track structure.
+ */
+void tcx_write_to_file (const char *filename, const Track * track);
+
+/**
+ * @brief Free memory at the end of the process.
+ *
+ * @param[in] track Pointer to the track structure.
+ */
+void tcx_free (Track * track);
+
+/**
+ * @todo Consider adding metadata, e.g., device name, to the file.
+ * @note Not yet implemented.
+ */
+void tcx_add_metadata ();
 
 #endif // TCXWRITER_H
